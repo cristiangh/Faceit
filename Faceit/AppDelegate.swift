e3ad8a933c5e2afada138a56169458fbcf91e242
Cristian <Cristian@imac.local>
2017-05-27 21:02:25 +0200

    lecture 4

--- Faceit/AppDelegate.swift
+++ Faceit/AppDelegate.swift
@@ -2,10 +2,10 @@
 //  AppDelegate.swift
 //  Faceit
 //
-//  Created by Cristian Lucania on 12/03/17.
-//  Copyright © 2017 Cristian Lucania. All rights reserved.
+//  Created by Cristian on 12/03/17.
+//  Copyright © 2017 Cristian. All rights reserved.
 //
-
+ 
 import UIKit
 
 @UIApplicationMain
--- Faceit/ViewController.swift
+++ Faceit/ViewController.swift
@@ -2,24 +2,15 @@
 //  ViewController.swift
 //  Faceit
 //
-//  Created by Cristian Lucania on 12/03/17.
-//  Copyright © 2017 Cristian Lucania. All rights reserved.
+//  Created by Cristian on 12/03/17.
+//  Copyright © 2017 Cristian. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
